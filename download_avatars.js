daa5d715144d9d88e7e0fb2f9368fa9e5cf092d5
Thofeeq <thofeeqm@gmail.com>
2018-09-03 23:50:13 +0000

    working on getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,22 @@
-var requestHandle = require('request');
+var request = require('request');
+var GITHUB_TOKEN = require('./secrets');
 
 console.log('Welcome to the Github Avatar Downloader');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'GITHUB_TOKEN',
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
