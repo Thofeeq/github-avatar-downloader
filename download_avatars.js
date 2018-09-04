795b61513695430758ac2066eccc2f8bc3ad4228
Thofeeq <thofeeqm@gmail.com>
2018-09-04 02:10:08 +0000

    Added 2 more functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var GITHUB_TOKEN = require('./secrets');
+var fs = require('fs');
+var getToken= require('./secrets');
 
 console.log('Welcome to the Github Avatar Downloader');
 
@@ -8,15 +9,53 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'GITHUB_TOKEN',
+      'Authorization': "token " + getToken.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    console.log("Requesting the images...");
+    cb(err, body, downloadImageByURL);
+    
   });
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+function downloadImageByURL(url,filePath) {
+
+  // for (let index = 0; index < urlArray.length; index++) {
+    request.get(url)      
+    .on('error', function (err) { 
+      if(err)
+      {
+       throw err;
+      }
+      else{
+        console.log("Contacting the server...")
+      }                                
+       
+    })
+    .on('response', function (response) { 
+      
+      response.on('end',function(){
+        console.log('Download Completed');
+      } )
+
+     })
+    .pipe(fs.createWriteStream("avatar/"+filePath+".png")); 
+    
+  //}
+  
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result, downloadImageCb) {
+  
   console.log("Errors:", err);
-  console.log("Result:", result);
-});
+  arrayOfContributorsObj = JSON.parse(result);
+  arrayOfContributorsObj.forEach(function(contributor){
+    downloadImageCb(contributor.avatar_url, contributor.login);
+  }
+);
+}
+
+);
